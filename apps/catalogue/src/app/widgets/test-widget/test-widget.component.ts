cfc56202ce49d15c39479baf9c73286a9ba4a438
Marko Berger <marko.berger@asseco-see.hr>
2021-08-23 19:01:22 +0200

    query params

--- apps/catalogue/src/app/widgets/test-widget/test-widget.component.ts
+++ apps/catalogue/src/app/widgets/test-widget/test-widget.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ng-mono360-test-widget',
   templateUrl: './test-widget.component.html',
-  styleUrls: ['./test-widget.component.css']
+  styleUrls: ['./test-widget.component.css'],
 })
 export class TestWidgetComponent implements OnInit {
-
-  constructor() { }
+  id: string;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((param) => {
+      this.id = param.id;
+    });
   }
-
 }
